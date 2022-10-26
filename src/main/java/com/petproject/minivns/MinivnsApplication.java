7ad3f415a3c22bd0f69bdb7661ba96386e142e96
redminter <oleg.a.fedyshyn@gmail.com>
2022-10-26 21:37:02 +0300

    added skeleton of project

---
+++ src/main/java/com/petproject/minivns/MinivnsApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.minivns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinivnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinivnsApplication.class, args);
+	}
+
+}
