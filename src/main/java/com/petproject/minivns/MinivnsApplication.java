13443a2ad995c57159083c2ac56674c3adf6e62a
redminter <oleg.a.fedyshyn@gmail.com>
2022-11-01 22:36:40 +0200

    checking for adding new role

--- src/main/java/com/petproject/minivns/MinivnsApplication.java
+++ src/main/java/com/petproject/minivns/MinivnsApplication.java
@@ -1,7 +1,11 @@
 package com.petproject.minivns;
 
+import com.petproject.minivns.entities.Role;
+import com.petproject.minivns.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MinivnsApplication {
@@ -9,5 +13,13 @@ public class MinivnsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MinivnsApplication.class, args);
 	}
+//	@Bean
+//	public CommandLineRunner run (RoleRepository roleRepository){
+//		return (args  ->{
+//			Role role = new Role("all");
+//			roleRepository.save(role);
+//			System.out.println(roleRepository.findAll());
+//		});
+//	}
 
 }
