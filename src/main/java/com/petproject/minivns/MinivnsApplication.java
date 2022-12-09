e4c88ee40d959f71fe69ba1e0ada39bda842d47d
redminter <oleg.a.fedyshyn@gmail.com>
2022-12-09 18:16:05 +0200

    -m

--- src/main/java/com/petproject/minivns/MinivnsApplication.java
+++ src/main/java/com/petproject/minivns/MinivnsApplication.java
@@ -1,11 +1,15 @@
 package com.petproject.minivns;
 
 
+import com.petproject.minivns.repositories.TaskRepository;
+import com.petproject.minivns.service.TaskService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class MinivnsApplication {
@@ -13,12 +17,12 @@ public class MinivnsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MinivnsApplication.class, args);
 	}
-	@Transactional
-	@Bean
-	public CommandLineRunner run (){
-		return (args  ->{
-
-		});
-	}
+//	@Bean
+//	public CommandLineRunner run (TaskService taskRepository){
+//		return (args  ->{
+//			System.out.println(taskRepository.getAllByUser_id(5));
+//			System.out.println(taskRepository.getAllBySubject_id(5, 7));
+//		});
+//	}
 
 }
