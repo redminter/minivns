b1e0860ac89d0223c9ac8b3c2d671051a40f57ba
redminter <oleg.a.fedyshyn@gmail.com>
2023-01-14 14:13:58 +0200

    deleted userDetails implementation

--- src/main/java/com/petproject/minivns/entities/User.java
+++ src/main/java/com/petproject/minivns/entities/User.java
@@ -1,19 +1,15 @@
 package com.petproject.minivns.entities;
 
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
 @EqualsAndHashCode(of = {"id", "firstName", "lastName"})
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_start7")
     @SequenceGenerator(name = "users_start7", allocationSize = 1)
@@ -72,35 +68,5 @@ public class User implements UserDetails {
         this.lastName = lastName;
         this.email = email;
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(role_Id);
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 }
